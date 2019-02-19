3e01ffa344f472b2c8e74ed8ce5e6a9e46b7d408
Will Sheehan <williamksheehan@gmail.com>
2019-02-18 20:58:51 -0500

    added main script

---
+++ main.py
@@ -0,0 +1,380 @@
+#POLYATOMIC DEFINITIONS
+
+poly = '''Look at the Common Polyatomic Ions sheet to identify your polyatomic(s)'''
+
+two_poly = '''How to name your compound:
+Write the name of each polyatomic.
+'''
+
+acid_plus_poly_ate = '''How to name your compound:
+(Polyatomic root name + ic) + Acid'''
+
+acid_plus_poly_ite = '''How to name your compound:
+(Polyatomic root name + ous) + Acid'''
+
+metal_plus_poly_exception = '''How to name your compound:
+1st element name + Polyatomic name'''
+
+metal_plus_poly = '''How to name your compound:
+1st element name + (roman numeral of the metal's charge needed to make the total charges add to 0) + Polyatomic name'''
+ 
+nonmetal_plus_poly = '''How to name your compound:
+1st element name + Polyatomic name'''
+
+acid_plus_poly_ous = '''How to write your compound:
+1.Replace the polyatomic's suffix with "ite". Identify this polyatomic using the Common Polyatomic Ions Sheet.
+2.Write H, then write the Polyatomic's formula.
+3.Find the charge of the Polyatomic (indicated on the Common Polyatomic Ions sheet)
+4.Find the number of Hydrogens (+1 charge) that when added to the polyatomic's charge will equal 0.
+5.Write this number as a subscript after H. If the number is 1, do not write a subscript.
+'''
+
+acid_plus_poly_ic = '''How to write your compound:
+1.Replace the polyatomic's suffix with "ate". Identify this polyatomic using the common Polyatomic Ions Sheet.
+2.Write H, then write the Polyatomic's formula.
+3.Find the charge of the Polyatomic (indicated on the Common Polyatomic Ions sheet)
+4.Find the number of Hydrogens (+1 charge) that when added to the polyatomic's charge will equal 0.
+5.Write this number as a subscript after H. If the number is 1, do not write a subscript.
+'''
+
+no_acid_plus_poly_formula = '''How to write your compound:
+1.Write the first element's symbol down, followed by the Polyatomic's symbol.
+2.Find the charge of the first element (roman numeral) and the charge of the polyatomic (indicated on the Common Polyatomic Ions sheet).
+3.Figure out how many of the first element and how many of the polyatomic you need in order for their total charges to add up to 0.
+4.Write down the number as a subscript after its respective element. If the number is 1, do not write a subscript.
+5.NOTE: if you need more than one of the polyatomic, write the polyatomic's formula in parentheses, and the number you found as a subscript to the parentheses.
+'''
+no_acid_plus_poly_exception_one_formula = '''How to write your compound:
+1.Write the first element's symbol down, followed by the Polyatomic's symbol.
+2.The charge of the first element is +2. Find the charge of the polyatomic (indicated on the Common Polyatomic Ions sheet).
+3.Figure out how many of the first element and how many of the polyatomic you need in order for their total charges to add up to 0.
+4.Write down the number as a subscript after its respective element. If the number is 1, do not write a subscript.
+5.NOTE: if you need more than one of the polyatomic, write the polyatomic's formula in parentheses, and the number you found as a subscript to the parentheses.
+'''
+
+silver_plus_poly_formula = '''How to write your compound:
+1.Write the first element's symbol down, followed by the Polyatomic's symbol.
+2.The charge of the silver is +1. Find the charge of the polyatomic (indicated on the Common Polyatomic Ions sheet).
+3.Figure out how many silver and how many of the polyatomic you need in order for their total charges to add up to 0.
+4.Write down the number as a subscript after its respective element. If the number is 1, do not write a subscript.
+5.NOTE: if you need more than one of the polyatomic, write the polyatomic's formula in parentheses, and the number you found as a subscript to the parentheses.
+'''
+final = '''How to write your compound:
+1.Write the first element's symbol down, followed by the Polyatomic's symbol.
+2.Find the charge of the first element and the charge of the polyatomic (indicated on the Common Polyatomic Ions sheet).
+3.Figure out how many of the first element and how many of the polyatomic you need in order for their total charges to add up to 0.
+4.Write down each number as a subscript after its respective element. If the number is 1, do not write a subscript.
+5.NOTE: if you need more than one of the polyatomic, write the polyatomic's formula in parentheses, and the number you found as a subscript to the parentheses.
+'''
+
+cation_formula = '''How to write your compound:
+1.Write the symbol for the Polyatomic and the symbol for the second element. (take out "ide" to find the root word for that element)
+2.Find the charge of the Polyatomic (indicated on the Common Polyatomic Ions sheet) and the charge of the second element.
+3.Find how many of the Polyatomic and element you need in order for their total charges to add up to 0.
+4.Write down each number as a subscript after its respective element. If the number is 1, do not write a subscript.
+5.NOTE: if you need more than one of the polyatomic, write the polyatomic's formula in parentheses, and the number you found as a subcsript to the parentheses.
+'''
+
+prefixes = '''
+Note: If the first element has no subscript, do not write a prefix.
+
+SUBSCRIPT NUMBER - PREFIX:
+1 - mono
+2 - di
+3 - tri
+4 - tetra
+5 - penta
+6 - hexa
+7 - hepta
+8 - octa
+9 - nona
+10 - deca
+'''
+
+#NO POLYATOMIC DEFINITIONS
+
+acid_name = '''How to name your compound:
+(Hydro + 1st element root name + ic) + (Acid)'''
+
+ide_acid = '''How to name your compound:
+(Hydro + Polyatomic root name + ic) + Acid'''
+
+metal_plus_nonmetal_exception = '''How to name your compound:
+1st element name + 2nd element root name + ide'''
+
+metal_plus_nonmetal = '''How to name your compound:
+1st element name + (roman numeral of the metal's charge needed to make the total charges add to 0) + 2nd element root
+name + ide
+EXAMPLE: Fe2S3 = iron(III)sulfide. Since sodium's total charge is -6, we need iron's total charge to be +6. Therefore we can conclude the charge of one iron must be +3 (divide +6 by 2) which you then write as roman numerals in parentheses.
+'''
+
+nonmetal_plus_nonmetal = '''How to name your compound:
+(1st subscript prefix + 1st element name) + (2nd subscript prefix + 2nd element root name + ide)'''
+
+acid_formula = '''How to write your compound:
+1.Write H
+2.Take out "Hydro" and "ic" from the first word. This is the root word for an element.
+3.Write the symbol for this element after H
+4.Find the charge of this element based on its location in the periodic table.
+5.Find the number of Hydrogens (+1 charge) that when added to this element's charge will equal 0.
+6.Write this number as a subscript after H. If the number is 1, do not write a subscript.
+EXAMPLE: hydrosulfuric acid = hydro-sulfur-ic acid = sulfur acid = H2S. You need two hydrogen (+1 charge) and one sulfur (-2 charge) for the total charges to add to 0.
+'''
+
+basic_formula = '''How to write your compound:
+1.Write the symbol for each element down. (take out "ide" from the second element to find the root word for that element)
+2.Find the charge of the first element (roman numeral) and the charge of the second element.
+3.Find the number of each element that is needed in order for their total charges to add to 0.
+4.Write this number down as a subscript after its respective element. If the number is one, do not write a subscript.
+EXAMPLE: chromium(III)chloride = CrCl3 because you need one chromium (+3 charge in this case) and three chlorine (-1 charge) for the total charge to equal 0.
+'''
+
+two_nonmetals_formula = '''How to write your compound:
+1.Take out the prefix from each word, if there is one. Take out "ide" from the second word. These are the root words for an element. Identify each element on the period table.
+2.Write the symbol for each element down.
+3.Convert each prefix to a subscript using the chart below.
+4.Write each subscript after its respective element.
+EXAMPLE: tetraphosphorus trisulfide = P4S3 because tetra = 4, which you add as a subscript to P(phosphorus), and tri = 3, which you add a subscript to S(sulfur)
+'''
+two_poly_formula = '''How to write your compound:
+1.Write the symbol for each polyatomic down.
+2.Find the charge of each polyatomic (indicated on the Common Polyatomic Ions sheet)
+3.Find the number of each polyatomic you need in order for their total charges to add to 0.
+4.Write this number as a subscript to each polyatomic in parentheses. If the number is 1, do not write a subscript or use parentheses
+EXAMPLE: ammonium hydroxide = NH4OH because you need one ammonium (-1 charge) and one hydroxide (+1 charge) for the total charge to equal 0.
+'''
+
+metal_nonmetal = '''How to write your compound:
+1.Write the symbol for each element down. (take out "ide" from the second word to find the root word for that element)
+2.Find the charge of each element.
+3.Find the number of each element that is needed in order to have their total charges add to 0.
+4.Write this number down as a subscript after its respective element. If the number is one, do not write a subscript.
+EXAMPLE: potassium sulfide = K2S because you need two potassium (+1 charge) and one sulfer (-2 charge) for their charges to add to 0.
+'''
+metal_nonmetal_exceptions = '''How to write your compound:
+1.Write the symbol for each element down. (take out "ide" from the second word to find the root word for that element)
+2.Find the charge of each element.
+3.Find the number of each element that is needed in order to have their total charges add to 0.
+4.Write this number down as a subscript after its respective element. If the number is one, do not write a subscript.
+EXAMPLE: cadmium phosphide = Ca3P2 because you need three cadmium (+2 charge) and two phosphorous (-3 charge) for their charges to add to 0.
+'''
+exception_charges = '''The charge of Zinc and Cadmium is +2. The charge of Silver is +1'''
+
+ide_poly = '''How to write your compound:
+1.Write "H" for hydrogen, then write the polyatomic's symbol.
+2.Find the charge of the polyatomic.
+3.Find the number of hydrogen (+1 charge) that when added to the polyatomic's charge will equal 0.
+4.Write this number down as a subscript to the "H".
+'''
+
+prefixes2 = '''
+NOTE: If there is no prefix or the prefix is "mono", do not write a subscript for that element.
+
+SUBSCRIPT NUMBER - PREFIX:
+1 - mono
+2 - di
+3 - tri
+4 - tetra
+5 - penta
+6 - hexa
+7 - hepta
+8 - octa
+9 - nona
+10 - deca
+'''
+
+#PROGRAM
+
+a = 1
+
+print '''Use this program to either name a compound given its formula, or write its formula given its name
+'''
+
+print '''For all answers, type "yes" or "no", then hit enter
+'''
+
+while a == 1:
+    
+    x = raw_input('''Are you writing the name for a compound, given the formula?
+''')
+
+    #FORMULA TO NAME
+
+    if x == 'yes':
+        polyatomic = raw_input('''Do you have a polyatomic?
+''')
+        #POLYATOMIC
+        if polyatomic == 'yes':
+            two = raw_input('''Do you have two polyatomics? (Check the Cations in the Common Polyatomic Ions sheet)
+''')
+            if two == 'yes':
+                print poly
+                print two_poly
+            if two == 'no':
+                acid = raw_input('''Is your first element hydrogen?
+''')
+                if acid == 'yes':
+                    print poly
+                    ate = raw_input('''Does your polyatomic end in "ate"?
+''')
+                    if ate == 'yes':    
+                        print acid_plus_poly_ate
+                    if ate == 'no':
+                        ide = raw_input('''Does your polyatomic end in "ide"?
+''')
+                        if ide == 'yes':
+                            print ide_acid
+                        if ide == 'no':
+                            print acid_plus_poly_ite
+                if acid == 'no':
+                    combination = raw_input('''Do you have a metal + a polyatomic?
+''')
+                    if combination == 'yes':
+                        metal_type = raw_input('''Is your metal a transition metal?
+''')
+                        if metal_type == 'yes':
+                            exception = raw_input('''Is your metal either zinc, silver, or cadmium?
+''')
+                            if exception == 'yes':
+                                print poly
+                                print metal_plus_poly_exception
+                            if exception == 'no':
+                                print poly
+                                print metal_plus_poly
+                        if metal_type == 'no':
+                            print poly
+                            print metal_plus_poly_exception
+                    if combination == 'no':
+                            cation = raw_input('''Do you have ammonium or hydroniumm? (Cation polyatomics)
+''')
+                            if cation == 'no':
+                                print poly
+                                print nonmetal_plus_poly
+                            if cation == 'yes':
+                                print poly
+                                print '''How to name your compound:
+    Polyatomic name + (second element root name + ide)'''
+                                
+        #NO POLYATOMIC
+        if polyatomic == 'no':
+            acid = raw_input('''Is your first element hydrogen?
+''')
+            if acid == 'yes':
+                print acid_name
+            if acid == 'no':
+                combination = raw_input('''Do you have a metal + a nonmetal?
+''')
+                if combination == 'yes':
+                    metal_type = raw_input('''Is your metal a transition metal?
+''')
+                    if metal_type == 'yes':
+                        exception = raw_input('''Is your metal either zinc, silver, or cadmium?
+''')
+                        if exception == 'yes':
+                            print metal_plus_nonmetal_exception
+                        if exception == 'no':
+                            print metal_plus_nonmetal
+                    if metal_type == 'no':
+                        print metal_plus_nonmetal_exception
+                if combination == 'no':
+                    metal_type = raw_input('''Do you have a nonmetal + a nonmetal?
+''')
+                    if metal_type == 'yes':
+                        print nonmetal_plus_nonmetal
+                        print prefixes
+                    else:
+                        print "Bye"
+    #NAME TO FORMULA
+                        
+    if x == 'no':
+        cation = raw_input('''Do you have ammonium or hydronium (Cation Polyatomic)?
+''')
+        if cation == 'yes':
+            two = raw_input('''Do you have another polyatomic?
+''')
+            if two == 'yes':
+                print poly
+                print two_poly_formula
+            if two == 'no':
+                print cation_formula
+        if cation == 'no':
+                polyatomic = raw_input('''Do you have an Anion polyatomic? (Example: "sulfuric" is a polyatomic but "hydrosulfuric" is not)
+''')
+                if polyatomic == 'yes':
+                    acid = raw_input('''Do you have an acid?
+''')
+                    if acid == 'yes':
+                        ending = raw_input('''Does your polyatomic end in "ous"?
+''')
+                        if ending == 'yes':
+                            print acid_plus_poly_ous
+                        if ending == 'no':
+                            ide = raw_input('''Is your first word hydrogen?
+''')
+                            if ide == 'yes':
+                                print poly
+                                print ide_poly
+                            if ide == 'no':
+                                print acid_plus_poly_ic
+                    if acid == 'no':
+                        type = raw_input('''Do you have a metal?
+''')
+                        if type == 'yes':
+                            metal = raw_input('''Do you have a transition metal?
+''')    
+                            if metal == 'yes':
+                                exception = raw_input('''Do you have either Zinc or Cadmium?
+''')
+                                if exception == 'yes':
+                                    print poly
+                                    print no_acid_plus_poly_exception_one_formula
+                                if exception == 'no':
+                                    silver = raw_input('''Do you have silver?
+''')
+                                    if silver == 'yes':
+                                        print poly
+                                        print silver_plus_poly_formula
+                                    if silver == 'no':
+                                        print poly
+                                        print no_acid_plus_poly_formula
+                            if metal == 'no':
+                                print poly
+                                print final
+                        if type == 'no':
+                            print final
+                if polyatomic == 'no':
+                    acid = raw_input('''Do you have an acid?
+''')
+                    if acid == 'yes':
+                        print acid_formula
+                    if acid == 'no':
+                        combination = raw_input('''Do you have two nonmetals?
+''')
+                        if combination == 'yes':
+                            print two_nonmetals_formula
+                            print prefixes2
+                        if combination == 'no':
+                            transition = raw_input('''Do you have a transition metal?
+''')
+                            if transition == 'yes':
+                                exception = raw_input('''Do you have either Zinc, Cadmium, or Silver?
+''')
+                                if exception == 'yes':
+                                    print exception_charges
+                                    print metal_nonmetal_exceptions
+                                if exception == 'no':
+                                    print basic_formula
+                            if transition == 'no':
+                                print metal_nonmetal
+                                
+    repeat = raw_input('''
+Would you like to try again?
+''')
+    if repeat == 'yes':
+        a = 1
+    else:
+        a = 2
+
+                
+                
+            
